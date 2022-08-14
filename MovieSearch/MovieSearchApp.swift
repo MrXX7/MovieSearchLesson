335b30c36ce2defb0ae753039809aee0b1452234
Oncu <oohancan@gmail.com>
2022-08-14 18:26:31 +0300

    Initial Commit

---
+++ MovieSearch/MovieSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSearchApp.swift
+//  MovieSearch
+//
+//  Created by Oncu Can on 14.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
