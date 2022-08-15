d2f0feadf638488556e6f01ec4503c5d28ac2161
Oncu <oohancan@gmail.com>
2022-08-15 17:51:48 +0300

    add class FilmDetailViewModel func takeFilmDetail and struct FilmDetailsViewModel for FilmDetailViewModel

--- MovieSearch/ViewModel/FilmDetailViewModel.swift
+++ MovieSearch/ViewModel/FilmDetailViewModel.swift
@@ -6,3 +6,58 @@
 //
 
 import Foundation
+import SwiftUI
+
+class FilmDetailViewModel: ObservableObject {
+    @Published var filmDetaili: FilmDetailsViewModel?
+    
+    let downloaderClient = DownloaderClient()
+    
+    func takeFilmDetail(imdbId: String) {
+        downloaderClient.downloadFilmDetail(imdbId: imdbId, completion: { (result) in
+            switch result {
+            case .failure(let wrong):
+                print(wrong)
+            case .success(let filmDetail):
+                self.filmDetaili = FilmDetailsViewModel(detail: filmDetail)
+            }
+        })
+    }
+}
+
+struct FilmDetailsViewModel {
+    let detail: FilmDetail
+    
+    var title: String {
+        detail.title
+    }
+    
+    var poster: String {
+        detail.poster
+    }
+    
+    var year: String {
+        detail.year
+    }
+    
+    var imdbId: String {
+        detail.imdbId
+    }
+    
+    var director: String {
+        detail.director
+    }
+    
+    var writer: String {
+        detail.writer
+    }
+    
+    var awards: String {
+        detail.awards
+    }
+    
+    var plot: String {
+        detail.plot
+    }
+    
+}
