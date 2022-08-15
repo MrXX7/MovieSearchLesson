eac674551930acf3d0e76305ace23758cf59d8a0
Oncu <oohancan@gmail.com>
2022-08-15 10:41:40 +0300

    add class FilmListViewModel, func doMovieSearch

--- MovieSearch/ViewModel/FilmViewModel.swift
+++ MovieSearch/ViewModel/FilmViewModel.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import SwiftUI
+
+class FilmListViewModel: ObservableObject {
+    
+   @Published var movies = [Film]()
+    
+    let downloaderClient = DownloaderClient()
+    
+    func doMovieSearch(movieName: String) {
+        downloaderClient.downloadMovies(search: movieName) { (result) in
+            switch result {
+            case.failure(let wrong):
+                print(wrong)
+            case.success(let allMovie):
+                if let allMovie = allMovie {
+                    self.movies = allMovie
+                }
+            }
+        }
+    }
+}
