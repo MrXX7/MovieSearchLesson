b18ae8531aedee065c4714b19c5ae9b7dfced908
Oncu <oohancan@gmail.com>
2022-08-15 17:21:18 +0300

    add func downloadFilmDetail for DownloaderClient

--- MovieSearch/Service/DownloaderClient.swift
+++ MovieSearch/Service/DownloaderClient.swift
@@ -23,6 +23,20 @@ class DownloaderClient {
             completion(.success(movieAnswer.films))
         }.resume()
     }
+    func downloadFilmDetail(imdbId: String, completion: @escaping (Result<FilmDetail,DownloaderError>) -> Void) {
+        guard let url = URL(string: "https://www.omdbapi.com/?i=\(imdbId)&apikey=d1e130e3") else {
+            return completion(.failure(.wrongUrl))
+        }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data, error == nil else {
+                return completion(.failure(.dataDidntCome))
+            }
+            guard let incomingFilmDetail = try? JSONDecoder().decode(FilmDetail.self, from: data) else {
+                return completion(.failure(.dataCouldNot))
+            }
+            completion(.success(incomingFilmDetail))
+        }.resume()
+    }
 }
 
 enum DownloaderError: Error {
