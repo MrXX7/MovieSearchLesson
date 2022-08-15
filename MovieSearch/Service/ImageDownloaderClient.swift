9405bd28c311fc3c497520f7146a66396b6ada37
Oncu <oohancan@gmail.com>
2022-08-15 12:56:42 +0300

    add class ImageDownloaderClient, func downloadImages and SpecialImage

--- MovieSearch/Service/ImageDownloaderClient.swift
+++ MovieSearch/Service/ImageDownloaderClient.swift
@@ -7,4 +7,23 @@
 
 import Foundation
 
-
+class ImageDownloaderClient: ObservableObject {
+    
+   @Published var downloadedImage: Data?
+    
+    func downloadImages(url: String) {
+        
+        guard let imageUrl = URL(string: url) else {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: imageUrl) { (data, response, error) in
+            guard let data = data, error == nil else {
+                return
+            }
+            DispatchQueue.main.async {
+                self.downloadedImage = data
+            }
+        } .resume()
+    }
+}
